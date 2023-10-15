b5e782b68350fa0fbf29ad4301f3a16ac0321d2e
Ednganji <ednganji01@monccnb.ca>
2023-10-15 14:26:48 -0300

    ajout des statics

--- web_project/Skstore/models.py
+++ web_project/Skstore/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Brand(models.Model):
+    
+    name = models.CharField(max_length=100)
+    id = models.PositiveSmallIntegerField(default=0)
+    image = models.ImageField(upload_to='files/images')
+    
